8ea5d3aff185fa0ea7b2e94b450e07de8f2c167d
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-11 21:08:26 -0800

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from 0 to 9
+ * Return: ALways 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
