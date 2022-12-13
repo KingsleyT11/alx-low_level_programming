5f8d484bcf7cc0a5e57ae4bb34aaf6425af49293
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-13 15:30:31 -0800

    digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,19 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int last_digit;
 
-	if (ld < 0)
-		ld *= -1;
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 
-	_putchar(ld + '0');
-
-	return (0);
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 }
