2691357c3fccb756ec70b9f7946cc73943dd3e14
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-13 13:52:12 -0800

    last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: number
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchard(ld + '0'');
+
+	return (0);
+}
