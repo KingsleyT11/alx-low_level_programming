7aae1c7fa7a9838cd0441fe1b3ce998e2784f58f
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-13 13:32:59 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/** print_sign -print numbers sign
+ *
+ * Return: 1 if number is > zero
+ * 0 if number = zero
+ * -1 if number is < zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
