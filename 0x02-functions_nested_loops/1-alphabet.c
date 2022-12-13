c031077dafa190d92fbc6fde76dc92e9a5465f2a
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-13 10:02:35 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+}
