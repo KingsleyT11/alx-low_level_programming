22220638cea9016c9755c12f89ca888cae5b3f34
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-13 13:45:05 -0800

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - absolue value of int
+ * @t: the int
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
