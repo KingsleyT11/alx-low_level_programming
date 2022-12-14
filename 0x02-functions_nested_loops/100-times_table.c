7d4297a0625ac769945eebe64f37153eb74ad45f
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-14 10:34:59 -0800

    adv 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,10 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <=n; a++)
+		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			for (b =1; b <= n; b++)
+			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
 				_putchar(44);
