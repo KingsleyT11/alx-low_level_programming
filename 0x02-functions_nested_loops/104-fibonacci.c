eddef68ed1775407ee89f7eff0c43546d505a6a3
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-14 13:19:46 -0800

    adv5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 
 /**
- * numLength - finds and prints numbers
- * @num : operand number
- * Return: number of digits
+ * main - prints the first 98 fibonacci numbers
+ *
+ * Return: Always 0
  */
 
-int numLength(int num)
+int main(void)
 {
-	int length = 0;
+	int c, boolean, boolean2;
+	long int n1, n2, fn, n11, n22;
 
-	if (!num)
+	n1 = 1;
+	n2 = 2;
+	boolean = boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
-		return (1);
+		if (boolean)
+		{
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = fn;
+		}
+		else
+		{
+			if (boolean2)
+			{
+				n11 = n1 % 1000000000;
+				n22 = b2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
+		}
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
 	}
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
+	printf("\n");
+	return (0);
 }
