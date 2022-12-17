1564bba1693c84e676d2ca77292ed303d7ade2aa
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-17 02:15:21 -0800

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints num between 0 to 14
+ * 10 times
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+				_putchar('1');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
