2b149e08612c3742f8b19e2ce1b400d1ade3aa59
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-17 01:57:53 -0800

    print_num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints num from 0 - 9
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
