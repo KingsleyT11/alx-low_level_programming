de570cadd997080689d09bf47e183972b84e63b8
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-20 12:53:07 -0800

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,14 +8,14 @@
  */
 void print_array(int *a, int n)
 {
-	int t:
+	int t;
 
-		for (t = 0; t < n; t++)
-		{
-			printf("%d", a[t]);
-			if (t != n - 1)
-				printf(", ");
-		}
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n - 1)
+			printf(", ");
+	}
 
-		printf("\n");
+	printf("\n");
 }
