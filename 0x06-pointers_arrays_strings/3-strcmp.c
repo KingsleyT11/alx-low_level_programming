dce44030a8b6d06a71099ce658a4bfaa7b841731
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2022-12-25 08:45:10 -0800

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @sl: string 1
+ * @s2: string 2
+ * Return: the difference in s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = 0;
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+	a++;
+	}
+	return (0);
+}
