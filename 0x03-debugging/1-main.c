08998b2fbbe22da8bc13bceca6d4b36c03977ce0
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2023-01-01 15:00:40 -0800

    1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinte loop avoided! \\o/\n");
+
+	return (0);
+}
