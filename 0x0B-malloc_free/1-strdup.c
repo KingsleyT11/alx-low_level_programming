250d4b10cd38dfe9a21e6e0ef3b7120d4bc27d56
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2023-01-10 13:25:59 -0800

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdib.h>
+#include "holberton.h"
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to be dupiicated
+ *
+ * Return: pointer  to the copied string (success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicte == NULL)
+		return (NULL);
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+	return (duplicate);
+}
