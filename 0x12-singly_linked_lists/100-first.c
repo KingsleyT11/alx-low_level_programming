6ed4ab47af8d68d84bc86d4f7ac5ae0217e2561e
KingsleyT11 <kingsley.tawiah1234@gmail.com>
2023-01-26 12:34:42 -0800

    adv 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-
-void __attribute__((constructor)) hare(void);
+#include<stdio.h>
 
 /**
- * hare - Prints a string before the
- *        main function is executed.
+ * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+ *   is executed before main()
  */
-void hare(void)
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
